b50a003e132b52a165884771b06a282d8160d44d
mess42 <>
2020-10-11 18:37:59 +0200

    nmea reader bugfix

--- read_nmea.py
+++ read_nmea.py
@@ -1,19 +1,28 @@
 import serial
 
 def raw_angle_to_decimal(raw):
-    int_abs = raw // 100
-    alpha = int_abs  + (raw - 100 * int_abs) / 60
+    alpha = float("nan")
+    if len(raw) > 0:
+        rawfloat = float(raw)
+        int_abs = rawfloat // 100
+        alpha = int_abs  + (rawfloat - 100 * int_abs) / 60
     return alpha
+
+def raw_to_float(raw):
+    flo = float("nan")
+    if len(raw) > 0:
+        flo = float(raw)
+    return flo
 
 def decode_gprmc_sentence(sentence):
     data = sentence[:-3].split(",")[1:]
     
     dec = {"time": data[0],
            "is_active": ( data[1] == "A" ),
-           "latitude": raw_angle_to_decimal( float(data[2]) ) * (1 - 2 * (data[3] == "S") ),
-           "longitude": raw_angle_to_decimal( float(data[4]) ) * (1 - 2 * (data[5] == "W") ),
-           "speed": float(data[6]) * 1.852 / 3.6,
-           "azimuth": float(data[7]),
+           "latitude":  raw_angle_to_decimal( data[2] ) * (1 - 2 * (data[3] == "S") ),
+           "longitude": raw_angle_to_decimal( data[4] ) * (1 - 2 * (data[5] == "W") ),
+           "speed": raw_to_float(data[6]) * 1.852 / 3.6,
+           "azimuth": raw_to_float(data[7]),
            "date": data[8],
            }
     return dec
@@ -30,7 +39,7 @@ ser.isOpen()
 
 
 for i in range(20):
-    sentence = ser.readline()
+    sentence = ser.readline().decode("utf-8")
     print(sentence)
     if sentence.startswith("$GPRMC"):
         print( decode_gprmc_sentence(sentence) )
