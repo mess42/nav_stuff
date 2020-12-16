7f4758068097c3001fa1c7a6c1cb8818740304bd
foerster <foerster@localhost>
2020-12-16 14:45:45 +0100

    director provider stub

---
+++ providers/directions.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This file defines direction providers.
+A direction provider converts a route to message signs and speech.
+"""
+
+import calc.angles
+import widgets.direction_icons
+
+def get_mapping_of_names_to_classes():
+    """
+    @brief: Pointers to all classes that shall be usable.
+    
+    @return d (dict)
+    """
+    d = {
+        "Director": Director,
+        "CarDirector": CarDirector,
+        }
+    return d
+
+
+def english_dicts():
+    verbs = {
+       "arrive"     :"arrive",
+       "continue"   :"turn",
+       "depart"     :"depart",
+       "end of road":"at the end of the road, turn",
+       "exit rotary":"take the",
+       "new name"   :"continue",
+       "turn"       :"turn",
+       "fork"       :"fork",
+       "merge"      :"merge",
+       "on ramp"    :"take the ramp to the",
+       "off ramp"   :"take the ramp on the",
+       "roundabout" :"enter the",
+       "rotary"     :"enter the",
+       "roundabout turn" : 	"at the roundabout, turn",
+       "exit roundabout":"take the",
+        }
+        
+    modifiers = {            
+       "arrive"     :"",
+       "continue"   :"{movement_modifier}",
+       "depart"     :"{nesw_after}",
+       "end of road":"{movement_modifier}",
+       "exit rotary":"{exit_number} exit",
+       "new name"   :"{movement_modifier}",
+       "turn"       :"{movement_modifier}",
+       "fork"       :"{movement_modifier}",
+       "merge"      :"",
+       "on ramp"    :"{movement_modifier}",
+       "off ramp"   :"{movement_modifier} and exit",
+       "roundabout" :"roundabout",
+       "rotary"     :"rotary",
+       "roundabout turn" : 	"{movement_modifier}",
+       "exit roundabout":"{exit_number} exit",
+        }
+    
+    modifiers2 = {
+        "uturn": "U-turn",
+        "sharp right"  : "sharp right",
+        "right"        : "right",
+        "slight right" : "slight right",
+        "straight"     : "straight",
+        "slight left"  : "slight left",
+        "left"         : "left",
+        "sharp left"   : "sharp left",    
+        }
+    
+    to_prepositions = {
+       "arrive"          : "at",
+       "continue"        : "to",
+       "depart"          : "to",
+       "end of road"     : "to",
+       "new name"        : "to",
+       "turn"            : "to",
+       "fork"            : "to",
+       "merge"           : "on",
+       "on ramp"         : "onto",
+       "off ramp"        : "to",
+       "roundabout"      : "",
+       "exit roundabout" : "to",
+       "roundabout turn" : "to",
+       "rotary"          : "",
+       "exit rotary"     : "to",
+        }
+    
+    street_name_after = {
+       "arrive"     :"{street_name_after}",
+       "continue"   :"{street_name_after}",
+       "depart"     :"{street_name_after}",
+       "end of road":"{street_name_after}",
+       "exit rotary":"{street_name_after}",
+       "new name"   :"{street_name_after}",
+       "turn"       :"{street_name_after}",
+       "fork"       :"{street_name_after}",
+       "merge"      :"{street_name_after}",
+       "on ramp"    :"{street_name_after}",
+       "off ramp"   :"{street_name_after}",
+       "roundabout" :"",
+       "rotary"     :"",
+       "roundabout turn" : 	"{street_name_after}",
+       "exit roundabout":"{street_name_after}",
+        }
+    
+    ordinals = {
+       1:  "first",
+       2:  "second",
+       3:  "third",
+       4:  "4th",
+       5:  "5th",
+       6:  "6th",
+       7:  "7th",
+       8:  "8th",
+       9:  "9th",
+       10: "10th"
+       }
+    
+    dicts = { "verbs"            : verbs, 
+              "modifiers"        : modifiers, 
+              "modifiers2"       : modifiers2,
+              "to_prepositions"  : to_prepositions, 
+              "street_name_after": street_name_after,
+              "ordinals"         : ordinals
+            }
+    
+    return dicts
+
+
+class Director(object):
+    def __init__(self, maneuvers, language_dicts = english_dicts() ):
+        """
+        @param maneuvers (list of dicts)
+        @param language_dicts (dict of dicts)
+        """
+        self.maneuvers = maneuvers
+        self.language_dicts = language_dicts
+    
+    def get_icon_class_by_name(self,name):
+        d = { "arrive"       : widgets.direction_icons.CheckerFlag,
+              "crossing"     : widgets.direction_icons.Crossing,
+              "nesw_arrow"   : widgets.direction_icons.NESWArrow,
+              "notification" : widgets.direction_icons.Notification,
+            }
+        return d["name"]
+    
+    def maneuver_to_text_blocks(self, maneuver):        
+
+        # get the first draft        
+        typ = maneuver["type"]
+        blocks = {"verb": self.language_dicts["verbs"][typ],
+                 "verb_modifier": self.language_dicts["modifiers"][typ],
+                 "to_preposition": self.language_dicts["to_prepositions"][typ],
+                 "street_name_after": self.language_dicts["street_name_after"][typ]
+                 }
+
+        # replace {variables} by maneuver data
+        text_replacement_dict = {
+           "{street_name_after}": maneuver["street_name_after"],
+           "{movement_modifier}": "{" + maneuver["movement_modifier"] +"}",
+           "{exit_number}"      : self.language_dicts["ordinals"][maneuver["exit_number"]],
+           "{nesw_after}"       : calc.angles.azimuth_to_nesw_string( azim_deg = maneuver["bearing_after_deg"] )
+            }
+        for key in self.language_dicts["modifiers2"]:
+            text_replacement_dict["{"+ key + "}"] = self.language_dicts["modifiers2"][key]
+            
+        for block_name in blocks:
+            for key in text_replacement_dict:
+                blocks[block_name] = blocks[block_name].replace( key, text_replacement_dict[key] )        
+        
+        # do the final polishing
+        if blocks["street_name_after"] == "":
+            blocks["to_preposition"] = ""
+    
+        return blocks
+
+
+class CarDirector(Director):
+    pass
--- providers/route.py
+++ providers/route.py
@@ -23,20 +23,6 @@ def get_mapping_of_names_to_classes():
     return d
 
 
-def ordinal(n):
-    nth = ""
-    if n == 1:
-        nth = "first"
-    elif n == 2:
-        nth = "second"
-    elif n == 3:
-        nth = "third"
-    else:
-        nth = str(n) +"th"
-    # there shouldn't be a roundabout with more than 20 exits
-    return nth
-
-
 class Router(object):
     def __init__(self, url_template= "", waypoints=[]):
         """
@@ -149,161 +135,47 @@ class OSRM(Router):
                         geo_before  = maneuvers[-1]["geometry_after"]
                         dist_before = maneuvers[-1]["distance_after"]
 
-                    icon_type = icon_types[ step["maneuver"]["type"] ]
+                    typ = step["maneuver"]["type"]
+
+                    modifier = ""
+                    if "modifier" in step["maneuver"]:
+                        modifier = step["maneuver"]["modifier"]
+                        if modifier == "uturn":
+                            typ = "uturn"
+                        if typ == "turn" and modifier == "straight":
+                            typ = "straight"
+                
                     bearings_deg = step["intersections"][0]["bearings"]
 
                     in_bearing_deg = None
-                    if"in" in step["intersections"][0]:
+                    if "in" in step["intersections"][0]:
                         in_bearing_deg = bearings_deg[ step["intersections"][0]["in"] ]
 
                     out_bearing_deg = None
-                    if"out" in step["intersections"][0]:
+                    if "out" in step["intersections"][0]:
                         out_bearing_deg = bearings_deg[ step["intersections"][0]["out"] ]
 
+                    exit_number = None
+                    if "exit" in step["maneuver"]:
+                        exit_number = step["maneuver"]["exit"]
+                    
                     maneuver = {
-                       "location"       : step["intersections"][0]["location"],
-                       "bearings_deg"   : bearings_deg,
-                       "in_bearing_deg" : in_bearing_deg,
-                       "out_bearing_deg": out_bearing_deg,
-                       "icon_type"      : icon_type,
-                       "left_driving"   : ( step["driving_side"].upper() == "LEFT" ),
-                       "textblocks"     : self.__step_to_text_blocks(step),
-                       "geometry_before": geo_before,
-                       "geometry_after" : step["geometry"]["coordinates"],
-                       "distance_before": dist_before,
-                       "distance_after" : step["distance"]
+                       "location"          : step["intersections"][0]["location"],
+                       "bearings_deg"      : bearings_deg,
+                       "in_bearing_deg"    : in_bearing_deg,
+                       "out_bearing_deg"   : out_bearing_deg,
+                       "icon_type"         : icon_types[ typ ],
+                       "left_driving"      : ( step["driving_side"].upper() == "LEFT" ),
+                       "type"              : typ,
+                       "street_name_after" : step["name"],
+                       "movement_modifier" : modifier,
+                       "exit_number"       : exit_number,
+                       "geometry_before"   : geo_before,
+                       "geometry_after"    : step["geometry"]["coordinates"],
+                       "distance_before"   : dist_before,
+                       "distance_after"    : step["distance"]
                         }
+                    
                     maneuvers += [ maneuver ]
 
         return maneuvers
-                    
-
-    def __step_to_text_blocks(self, step, distance_in_m = None):
-
-        verbs = {
-           "arrive"     :"arrive",
-           "continue"   :"continue",
-           "depart"     :"depart",
-           "end of road":"at the end of the road, turn",
-           "exit rotary":"take the",
-           "new name"   :"continue",
-           "turn"       :"turn",
-           "fork"       :"fork",
-           "merge"      :"merge",
-           "on ramp"    :"take the ramp to the",
-           "off ramp"   :"take the ramp on the",
-           "roundabout" :"enter the",
-           "rotary"     :"enter the",
-           "roundabout turn" : 	"at the roundabout, turn",
-           "exit roundabout":"take the",
-            }
-            
-        modifiers = {            
-           "arrive"     :"",
-           "continue"   :"{modifier}",
-           "depart"     :"{nesw_after}",
-           "end of road":"{modifier}",
-           "exit rotary":"{exit_number} exit",
-           "new name"   :"{modifier}",
-           "turn"       :"{modifier}",
-           "fork"       :"{modifier}",
-           "merge"      :"",
-           "on ramp"    :"{modifier}",
-           "off ramp"   :"{modifier} to exit",
-           "roundabout" :"roundabout",
-           "rotary"     :"rotary",
-           "roundabout turn" : 	"{modifier}",
-           "exit roundabout":"{exit_number} exit",
-            }
-        
-        to_prepositions = {
-           "arrive"     :"at",
-           "continue"   :"on",
-           "depart"     :"to",
-           "end of road":"to",
-           "exit rotary":"to",
-           "new name"   :"to",
-           "turn"       :"to",
-           "fork"       :"to",
-           "merge"      :"on",
-           "on ramp"    :"onto",
-           "off ramp"   :"to",
-           "roundabout" :"",
-           "rotary"     :"",
-           "roundabout turn" : 	"to",
-           "exit roundabout":"to",
-            }
-        
-        new_names = {
-           "arrive"     :"{name}",
-           "continue"   :"{name}",
-           "depart"     :"{name}",
-           "end of road":"{name}",
-           "exit rotary":"{name}",
-           "new name"   :"{name}",
-           "turn"       :"{name}",
-           "fork"       :"{name}",
-           "merge"      :"{name}",
-           "on ramp"    :"{name}",
-           "off ramp"   :"{name}",
-           "roundabout" :"",
-           "rotary"     :"",
-           "roundabout turn" : 	"{name}",
-           "exit roundabout":"{name}",
-            }
-        
-        text_replacement_dict = {
-           "{name}"       : step["name"],
-           "{modifier}"   :"".join(list(man["modifier"] for man in [step["maneuver"]] if"modifier" in man )),
-           "{exit_number}":"".join(list(ordinal(man["exit"]) for man in [step["maneuver"]] if"exit" in man)) ,
-           "{nesw_after}": calc.angles.azimuth_to_nesw_string(step["maneuver"]["bearing_after"])
-            }
-        
-        typ = step["maneuver"]["type"]
-        if typ not in verbs:
-            print(step)
-            raise Exception("TODO: implement" + step["maneuver"]["type"] )
-
-        blocks = {"verb": verbs[typ],
-                 "verb_modifier": modifiers[typ],
-                 "to_preposition": to_prepositions[typ],
-                 "new_name": new_names[typ]
-                 }
-                
-        for block_name in blocks:
-            for key in text_replacement_dict:
-                blocks[block_name] = blocks[block_name].replace( key, text_replacement_dict[key] )        
-        
-        # Corrections
-        if blocks["verb_modifier"] == "uturn":
-            blocks["verb"]          = "make a"
-            blocks["verb_modifier"] = "U-turn"
-        if blocks["new_name"] == "":
-            blocks["to_preposition"] = ""
-        if  blocks["verb_modifier"] == "straight":
-            blocks["verb"] = "continue"
-                  
-        return blocks
-
-
-if __name__ == "__main__":
-
-    waypoints = np.array([[13.388860,52.517037],[13.397634,52.529407],[13.428555,52.523219],[13.418555,52.523215]])
-
-    router = OSRM( url_template= "https://router.project-osrm.org/trip/v1/driving/{waypoints}?source=first&destination=last&steps=true&geometries=geojson")    
-    router.set_route(waypoints)
-    
-    lats_deg, lons_deg = router.get_polyline_of_whole_route()
-    
-"""        
-    for leg in trip["legs"]:
-        for step in leg["steps"]:
-            
-            line_from_maneuver_to_next_one = np.array(step["geometry"]["coordinates"])
-            
-            maneuver_loc = step["maneuver"]["location"]
-            
-            step_as_text = router.step_to_text(step)
-            
-            print(step_as_text,"and continue", step["distance"],"m")
-"""         
